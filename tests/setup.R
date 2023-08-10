3b0c18f7604235f1ade5a626ae597446a3e8ee4f
Anthony Damico <ajdamico@gmail.com>
2023-08-10 11:10:25 -0400

    sipp draft

--- tests/setup.R
+++ tests/setup.R
@@ -1,6 +1,9 @@
 # federal programs
 # poverty oversample
 # monthly dynamics
+variables_to_keep <-
+	c( 'ssuid' , 'pnum' , 'monthcode' , 'spanel' , 'swave' , 'erelrpe' , 'tlivqtr' , 'wpfinwgt' , 
+	'rmesr' , 'thcyincpov' , 'tfcyincpov' , 'tehc_st' , 'rhicovann' , 'rfpov' , 'thnetworth' )
 library(httr)
 library(data.table)
 
@@ -16,7 +19,7 @@ GET( main_url , write_disk( main_tf ) , progress() )
 
 main_csv <- unzip( main_tf , exdir = tempdir() )
 
-sipp_main_dt <- fread( main_csv , sep = "|" )
+sipp_main_dt <- fread( main_csv , sep = "|" , select = variables_to_keep )
 
 sipp_main_df <- data.frame( sipp_main_dt )
 
@@ -48,8 +51,6 @@ sipp_df <-
 	)
 	
 stopifnot( nrow( sipp_df ) == sum( sipp_rw_df[ , 'monthcode' ] %in% 12 ) )
-
-sipp_df[ , 'one' ] <- 1
 # sipp_fn <- file.path( path.expand( "~" ) , "SIPP" , "this_file.rds" )
 # saveRDS( sipp_df , file = sipp_fn , compress = FALSE )
 # sipp_df <- readRDS( sipp_fn )
@@ -64,52 +65,100 @@ sipp_design <-
 			rho = 0.5
 		)
 	
+rmesr_values <-
+	c( 
+		"With a job entire month, worked all weeks",
+		"With a job all month, absent from work without pay 1+ weeks, absence not due to layoff",
+		"With a job all month, absent from work without pay 1+ weeks, absence due to layoff",
+		"With a job at least 1 but not all weeks, no time on layoff and no time looking for work",
+		"With a job at least 1 but not all weeks, some weeks on layoff or looking for work",
+		"No job all month, on layoff or looking for work all weeks",
+		"No job all month, at least one but not all weeks on layoff or looking for work",
+		"No job all month, no time on layoff and no time looking for work"
+	)
+
 sipp_design <- 
 	
 	update( 
 		
 		sipp_design , 
 		
-		one = 1
+		one = 1 ,
+		
+		employment_status = factor( rmesr , levels = 1:8 , labels = rmesr_values ) ,
+			
+		household_below_poverty = as.numeric( thcyincpov < 1 ) ,
+		
+		family_below_poverty = as.numeric( tfcyincpov < 1 ) ,
+		
+		state_name =
+			
+			factor(
+				
+				as.numeric( tehc_st ) ,
+				
+				levels = 
+					c(1L, 2L, 4L, 5L, 6L, 8L, 9L, 10L, 
+					11L, 12L, 13L, 15L, 16L, 17L, 18L, 
+					19L, 20L, 21L, 22L, 23L, 24L, 25L, 
+					26L, 27L, 28L, 29L, 30L, 31L, 32L, 
+					33L, 34L, 35L, 36L, 37L, 38L, 39L, 
+					40L, 41L, 42L, 44L, 45L, 46L, 47L, 
+					48L, 49L, 50L, 51L, 53L, 54L, 55L, 
+					56L, 60L, 61L) ,
+		
+				labels =
+					c("Alabama", "Alaska", "Arizona", "Arkansas", "California", 
+					"Colorado", "Connecticut", "Delaware", "District of Columbia", 
+					"Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", 
+					"Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
+					"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
+					"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
+					"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", 
+					"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+					"South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", 
+					"Washington", "West Virginia", "Wisconsin", "Wyoming", "Puerto Rico",
+					"Foreign Country")
+			)
 			
 	)
 sum( weights( sipp_design , "sampling" ) != 0 )
 
-svyby( ~ one , ~ state , sipp_design , unwtd.count )
+svyby( ~ one , ~ state_name , sipp_design , unwtd.count )
 svytotal( ~ one , sipp_design )
 
-svyby( ~ one , ~ state , sipp_design , svytotal )
-svymean( ~ first_fed_formula , sipp_design , na.rm = TRUE )
+svyby( ~ one , ~ state_name , sipp_design , svytotal )
+svymean( ~ tftotinc , sipp_design , na.rm = TRUE )
 
-svyby( ~ first_fed_formula , ~ state , sipp_design , svymean , na.rm = TRUE )
-svymean( ~ sex , sipp_design , na.rm = TRUE )
+svyby( ~ tftotinc , ~ state_name , sipp_design , svymean , na.rm = TRUE )
+svymean( ~ employment_status , sipp_design , na.rm = TRUE )
 
-svyby( ~ sex , ~ state , sipp_design , svymean , na.rm = TRUE )
-svytotal( ~ first_fed_formula , sipp_design , na.rm = TRUE )
+svyby( ~ employment_status , ~ state_name , sipp_design , svymean , na.rm = TRUE )
+svytotal( ~ tftotinc , sipp_design , na.rm = TRUE )
 
-svyby( ~ first_fed_formula , ~ state , sipp_design , svytotal , na.rm = TRUE )
-svytotal( ~ sex , sipp_design , na.rm = TRUE )
+svyby( ~ tftotinc , ~ state_name , sipp_design , svytotal , na.rm = TRUE )
+svytotal( ~ employment_status , sipp_design , na.rm = TRUE )
 
-svyby( ~ sex , ~ state , sipp_design , svytotal , na.rm = TRUE )
-svyquantile( ~ first_fed_formula , sipp_design , 0.5 , na.rm = TRUE )
+svyby( ~ employment_status , ~ state_name , sipp_design , svytotal , na.rm = TRUE )
+svyquantile( ~ tftotinc , sipp_design , 0.5 , na.rm = TRUE )
 
 svyby( 
-	~ first_fed_formula , 
-	~ state , 
+	~ tftotinc , 
+	~ state_name , 
 	sipp_design , 
 	svyquantile , 
 	0.5 ,
 	ci = TRUE , na.rm = TRUE
 )
 svyratio( 
-	numerator = ~ bf_exclr06 , 
-	denominator = ~ bf_endr06 , 
+	numerator = ~ tftotinc , 
+	denominator = ~ rfpov , 
 	sipp_design ,
 	na.rm = TRUE
 )
-sub_sipp_design <- subset( sipp_design , p_utdpol == 1 )
-svymean( ~ first_fed_formula , sub_sipp_design , na.rm = TRUE )
-this_result <- svymean( ~ first_fed_formula , sipp_design , na.rm = TRUE )
+sub_sipp_design <- subset( sipp_design , rhicovann == 1 )
+svymean( ~ tftotinc , sub_sipp_design , na.rm = TRUE )
+this_result <- svymean( ~ tftotinc , sipp_design , na.rm = TRUE )
 
 coef( this_result )
 SE( this_result )
@@ -118,8 +167,8 @@ cv( this_result )
 
 grouped_result <-
 	svyby( 
-		~ first_fed_formula , 
-		~ state , 
+		~ tftotinc , 
+		~ state_name , 
 		sipp_design , 
 		svymean ,
 		na.rm = TRUE 
@@ -130,22 +179,22 @@ SE( grouped_result )
 confint( grouped_result )
 cv( grouped_result )
 degf( sipp_design )
-svyvar( ~ first_fed_formula , sipp_design , na.rm = TRUE )
+svyvar( ~ tftotinc , sipp_design , na.rm = TRUE )
 # SRS without replacement
-svymean( ~ first_fed_formula , sipp_design , na.rm = TRUE , deff = TRUE )
+svymean( ~ tftotinc , sipp_design , na.rm = TRUE , deff = TRUE )
 
 # SRS with replacement
-svymean( ~ first_fed_formula , sipp_design , na.rm = TRUE , deff = "replace" )
-svyciprop( ~ dtap_3p , sipp_design ,
+svymean( ~ tftotinc , sipp_design , na.rm = TRUE , deff = "replace" )
+svyciprop( ~ family_below_poverty , sipp_design ,
 	method = "likelihood" )
-svyttest( first_fed_formula ~ dtap_3p , sipp_design )
+svyttest( tftotinc ~ family_below_poverty , sipp_design )
 svychisq( 
-	~ dtap_3p + sex , 
+	~ family_below_poverty + employment_status , 
 	sipp_design 
 )
 glm_result <- 
 	svyglm( 
-		first_fed_formula ~ dtap_3p + sex , 
+		tftotinc ~ family_below_poverty + employment_status , 
 		sipp_design 
 	)
 
@@ -156,12 +205,12 @@ svymean( ~ factor( findInterval( thnetworth , c( 1 , 5000 , 10000 , 25000 , 5000
 library(convey)
 sipp_design <- convey_prep( sipp_design )
 
-svygini( ~ vd4020n , sipp_design , na.rm = TRUE )
+svygini( ~ tftotinc , sipp_design , na.rm = TRUE )
 library(srvyr)
 sipp_srvyr_design <- as_survey( sipp_design )
 sipp_srvyr_design %>%
-	summarize( mean = survey_mean( first_fed_formula , na.rm = TRUE ) )
+	summarize( mean = survey_mean( tftotinc , na.rm = TRUE ) )
 
 sipp_srvyr_design %>%
-	group_by( state ) %>%
-	summarize( mean = survey_mean( first_fed_formula , na.rm = TRUE ) )
+	group_by( state_name ) %>%
+	summarize( mean = survey_mean( tftotinc , na.rm = TRUE ) )
